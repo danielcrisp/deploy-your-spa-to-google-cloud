a12ee7a6dadc1f8167f8dc07898cd9f67e59adfb
Daniel Crisp <daniel@danielcrisp.com>
2018-06-13 15:12:09 +0100

    Wire up the routes

--- gcp-demo/src/app/app-routing.module.ts
+++ gcp-demo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RouteOneComponent } from './route-one/route-one.component';
+import { RouteTwoComponent } from './route-two/route-two.component';
+
+const routes: Routes = [{
+  path: 'one',
+  component: RouteOneComponent
+}, {
+  path: 'two',
+  component: RouteTwoComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
