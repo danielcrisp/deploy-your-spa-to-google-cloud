64b6b8e8504c4b5952646d881d22cc8a58d30667
Daniel Crisp <daniel@danielcrisp.com>
2018-06-13 15:20:07 +0100

    Add serve-static

--- gcp-demo/dist/index.js
+++ gcp-demo/dist/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const serveStatic = require('serve-static');
+
 const app = express();
+
+app.use(serveStatic(__dirname + '/gcp-demo'));
 
 // Check if the host env has specified a port to run on
 const port = process.env.PORT || 3000;
