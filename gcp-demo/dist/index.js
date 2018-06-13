519fdb768037449a58f53e486cc32e14eab88447
Daniel Crisp <daniel@danielcrisp.com>
2018-06-13 15:20:52 +0100

    Route all non-static traffic to the index file

--- gcp-demo/dist/index.js
+++ gcp-demo/dist/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(serveStatic(__dirname + '/gcp-demo'));
 
+app.use('*', (req, res) => {
+  res.sendFile(__dirname + '/gcp-demo/index.html');
+});
+
 // Check if the host env has specified a port to run on
 const port = process.env.PORT || 3000;
 
