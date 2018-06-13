ea5524e6545afbac42ca5b74c3b59b50b003dafe
Daniel Crisp <daniel@danielcrisp.com>
2018-06-13 15:21:20 +0100

    Add compression

--- gcp-demo/dist/index.js
+++ gcp-demo/dist/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const compression = require('compression');
 
 const app = express();
+
+app.use(compression());
 
 app.use(serveStatic(__dirname + '/gcp-demo'));
 
